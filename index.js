ece6c3cd2dbcb3b5386883d17c14a0a49fd4225c
Mujster <mmujtabach1234@gmail.com>
2024-12-05 00:01:06 +0500

    Model staged

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const LivestockRoutes=require('./routes/livestock/utility');
 const ProductRoutes=require('./routes/product/utility');
 const PriceListRoutes=require('./routes/priceList/utility');
 
-// mongoose.connect('mongodb://localhost:27017/marketprice', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
-// .then(() => console.log('Connected to MongoDB'))
-// .catch(err => console.error('Could not connect to MongoDB', err));
+mongoose.connect('mongodb://localhost:27017/marketprice', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
 
 app.get('/', (req, res) => res.send('Market Price Analytics Service !'));
 
